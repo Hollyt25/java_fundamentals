eab20c4dc2e32022081fe0194072acd41d6e597a
Holly <Hollyt25@gmail.com>
2022-08-07 19:40:49 -0500

    Completed Exercises

--- src/labs_examples/arrays/labs/Exercise_07.java
+++ src/labs_examples/arrays/labs/Exercise_07.java
@@ -1,5 +1,7 @@
 package labs_examples.arrays.labs;
 
+import java.util.ArrayList;
+
 /**
  *  ArrayLists
  *
@@ -10,4 +12,29 @@ package labs_examples.arrays.labs;
  *
  */
 public class Exercise_07 {
+    public static void main(String[] args) {
+
+        ArrayList<Integer> list = new ArrayList<Integer>();
+
+        int count = 0;
+        for(int i=0; i<21; i++){
+            list.add(i, count);
+            count++;
+            System.out.println(list.get(i));
+        }
+
+      ArrayList<Integer> list2 = new ArrayList<Integer>();
+        list2 = (ArrayList)list.clone();
+        for(int i=0; i<list2.size(); i++){
+            System.out.println("copy of ArrayList list " + list2.get(i));
+        }
+
+        if(list.contains(5)){
+            System.out.println(list.indexOf(4));
+        }
+
+        System.out.println(list.isEmpty());
+
+
+    }
 }
