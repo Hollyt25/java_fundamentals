cf37335d3708727e80f4999fcc9f20b1e35708b8
Holly <Hollyt25@gmail.com>
2022-08-21 20:02:08 -0500

    Completed Exercises

--- src/labs_examples/objects_classes_methods/labs/oop/A_inheritance/Exercise_02.java
+++ src/labs_examples/objects_classes_methods/labs/oop/A_inheritance/Exercise_02.java
@@ -3,7 +3,7 @@ package labs_examples.objects_classes_methods.labs.oop.A_inheritance;
 /**
  * Run the class below and study the output. What prints and why?
  *
- * Your answer:
+ * Your answer: It prints out the value of i in the class A.  Because class B is the child class of the class A.  The value of i in class A is passed to the class B.
  *
  */
 class A {
